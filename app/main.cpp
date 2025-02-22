e19aff1465ab36666dc5d3ebfbc22b8e48c1f238
Cannafox <deuterpl@gmail.com>
2025-02-22 19:22:08 +0100

    init

--- app/main.cpp
+++ app/main.cpp
@@ -1,4 +1,3 @@
-/*#include <app_public_lib/main.hpp>*/
 #include <iostream>
 #include <app_public/application.hpp>
 int main(int argv, char* argc[]) {
--- src/application.cpp
+++ src/application.cpp
@@ -1,5 +1,4 @@
 #include <app_public/application.hpp>
-#include <iostream>
 
 Application::Application() {
   std::cout << "Initialize app " << std::endl; 
